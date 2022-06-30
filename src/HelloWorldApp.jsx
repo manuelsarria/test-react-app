6921498f6d8947cf1a2b442414e3a60dc8aa1859
Manuel Sarrria <manuelsarria@Manuels-MacBook-Pro.local>
2022-06-30 14:13:02 -0500

    update

---
+++ src/HelloWorldApp.jsx
@@ -0,0 +1,13 @@
+// export function App() {
+//     // document.createElement
+//     return  (<h1>Hola mundo!!!!</h1>);
+
+// }
+
+// export default App;
+
+export const HelloWorldApp = () => {
+  return (
+    <div>Hellow World App</div>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import ReactDOM from 'react-dom/client';
+import {HelloWorldApp}  from './HelloWorldApp'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+    <React.StrictMode>
+        <HelloWorldApp/> 
+    </React.StrictMode>  
+);
